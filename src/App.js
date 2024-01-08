5ab3b10e4c8f5e8ee6a5d7b6f0aed7cc75b53197
Elena Pashkova <89459474+oivie@users.noreply.github.com>
2024-01-08 11:52:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from './Main';
 import Catalogue from './Catalogue';
 import About from './About';
 import Footer from './Footer';
-
 
 function App() {
   return (
@@ -17,23 +11,22 @@ function App() {
       <Router>
         <nav className='container'>
           <Link to='/main' className='link'>Main</Link>
-          <Link to='/catalogue' className='link' >Catalogue</Link>
-          <Link to='/about' className='link' >About Us</Link>
+          <Link to='/catalogue' className='link'>Catalogue</Link>
+          <Link to='/about' className='link'>About Us</Link>
         </nav>
 
         <Routes>
-          <Route path='/main' element={ <Main /> } />
-          <Route path='/catalogue' element={ <Catalogue /> } />
-          <Route path='/about' element={ <About /> } />
+          <Route path='/' element={<Main />} /> {/* Catch-all route */}
+          <Route path='/main' element={<Main />} />
+          <Route path='/catalogue' element={<Catalogue />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </Router>
 
-      
-
       <Footer />
-
     </div>
   );
 }
 
 export default App;
+
